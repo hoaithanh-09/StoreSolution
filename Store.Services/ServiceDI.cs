fc245b8e06b389d01cc66e0000785ca73a086127
hoaithanh-09 <1851120014@sv.ut.edu.vn>
2022-05-25 00:20:02 +0700

    chores: fix appsetting

---
+++ Store.Services/ServiceDI.cs
@@ -0,0 +1,23 @@
+﻿using Castle.Core.Configuration;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Store.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Services
+{
+    public class ServiceDI
+    {
+        public static void AddDependencies(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddTransient<UserManager<User>, UserManager<User>>();
+            services.AddTransient<SignInManager<User>, SignInManager<User>>();
+            services.AddTransient<RoleManager<Role>, RoleManager<Role>>();
+
+        }
+    }
+}
